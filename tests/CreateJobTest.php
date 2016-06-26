12174436410d19b04f280479573cf74d67898a5f
booomerang <dusanea@gmail.com>
2016-06-26 20:08:38 +0300

    Add the basic easiest test for project.

--- tests/CreateJobTest.php
+++ tests/CreateJobTest.php
@@ -4,16 +4,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class CreateJobTest extends TestCase
 {
     /**
-     * A basic functional test example.
+     * A basic test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testJobCreatePageVisiting()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        $this->visit('/job/create')
+            ->see('Create');
     }
 }
