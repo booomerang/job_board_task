ba69218b00e9b845c268b5e1410caa70cf670d4d
booomerang <dusanea@gmail.com>
2016-06-26 17:46:12 +0300

    Added small library for flash messages.

--- config/app.php
+++ config/app.php
@@ -158,6 +158,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Laracasts\Flash\FlashServiceProvider::class
+
     ],
 
     /*
@@ -203,6 +205,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Flash' => Laracasts\Flash\Flash::class
 
     ],
 
